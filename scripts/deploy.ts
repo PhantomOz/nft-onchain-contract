f2bb95bcca6bc6c94fcfb71627fcbb4a936e457d
PhantomOz <faniogor@gmail.com>
2024-02-23 22:06:49 +0100

    This commit will add deploy script

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,21 +1,15 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const NAME = "Web3Bridge Students";
+  const SYMBOL = "W3BS";
 
-  const lockedAmount = ethers.parseEther("0.001");
+  const nft = await ethers.deployContract("ERC721Token", [NAME, SYMBOL]);
 
-  const lock = await ethers.deployContract("Lock", [unlockTime], {
-    value: lockedAmount,
-  });
-
-  await lock.waitForDeployment();
+  await nft.waitForDeployment();
 
   console.log(
-    `Lock with ${ethers.formatEther(
-      lockedAmount
-    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+    `deployed to ${nft.target}`
   );
 }
 
