dc343a31115d15235e84d28a115536b32e87a947
PhantomOz <faniogor@gmail.com>
2024-02-23 21:55:56 +0100

    This commit will add generateCharacter function

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -19,4 +19,22 @@ contract ERC721Token is ERC721 {
         _safeMint(msg.sender, s_tokenCounter);
         s_tokenCounter++;
     }
+
+    function generateCharacter(string memory _name) internal pure returns(string memory){
+        bytes memory svg = abi.encodePacked(
+            '<svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMinYMin meet" viewBox="0 0 350 350">',
+            '<style>.base { fill: white; font-family: serif; font-size: 14px; }</style>',
+            '<rect width="100%" height="100%" fill="black" />',
+            '<text x="50%" y="40%" class="base" dominant-baseline="middle" text-anchor="middle">',"",_name,'</text>',
+            '<text x="50%" y="50%" class="base" dominant-baseline="middle" text-anchor="middle">', "Web3Bridge CX Student",'</text>',
+            '</svg>'
+        );
+        return string(
+            abi.encodePacked(
+                "data:image/svg+xml;base64,",
+                Base64.encode(svg)
+            )    
+        );
+    }
+
 }
